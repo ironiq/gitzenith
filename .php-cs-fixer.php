7ca0ee8385066b1b72a78cc1a7a215cd3db5fca8
Krisztian VASAS <iron@ironiq.hu>
2024-11-01 05:01:29 +0100

    Changed coding style in php-cs-fixer config

--- .php-cs-fixer.php
+++ .php-cs-fixer.php
@@ -1,19 +1,22 @@
 <?php
 
 $finder = PhpCsFixer\Finder::create()
-    ->in(__DIR__ . '/src')
-    ->in(__DIR__ . '/tests')
+	->in(__DIR__ . '/src')
+	->in(__DIR__ . '/tests')
 ;
 
 $config = new PhpCsFixer\Config();
 return $config->setRules([
-        '@Symfony' => true,
-        'global_namespace_import' => [
-            'import_classes' => true,
-            'import_constants' => false,
-            'import_functions' => false,
-        ],
-    ])
-    ->setRiskyAllowed(true)
-    ->setFinder($finder)
+		'@Symfony' => true,
+		'global_namespace_import' => [
+			'import_classes' => true,
+			'import_constants' => false,
+			'import_functions' => false,
+		],
+		'no_spaces_after_function_name' => true,
+		'spaces_inside_parentheses' => ['space' => 'single',]
+	])
+	->setIndent("\t")
+	->setRiskyAllowed(true)
+	->setFinder($finder)
 ;
